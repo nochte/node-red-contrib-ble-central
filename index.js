c7ba4313e94cbe1ff6acff902db253472eec25d4
Nate Rowland <nochte@gmail.com>
2018-07-17 14:02:03 +0000

    packaging

---
+++ index.js
@@ -0,0 +1,7 @@
+const readerService = require('./lib/readerService');
+const communicationService = require('./lib/communicationService');
+
+module.exports = {
+  readerService,
+  communicationService
+};
